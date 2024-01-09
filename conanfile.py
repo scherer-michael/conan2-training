d5f7b24ebd922fcd07575ca512bfcf0f38030867
Michael <ms@mscherer.eu>
2024-01-09 21:11:43 +0100

    Fix test_package and conanfile.

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,7 @@ from conan.tools.build import check_max_cppstd, check_min_cppstd
 class trainingRecipe(ConanFile):
     name = "training"
     version = "1.0"
+    package_type = "application"
 
     # Optional metadata
     license = "MIT"
@@ -19,6 +20,8 @@ class trainingRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    
+    exports_sources = "CMakeLists.txt", "src/*", "tests/*"
 
     def requirements(self):
         self.requires("spdlog/1.12.0")
@@ -26,7 +29,7 @@ class trainingRecipe(ConanFile):
         self.test_requires("gtest/1.13.0")
 
     def build_requirements(self):
-        self.tool_requires("cmake/3.22.6")
+        self.tool_requires("cmake/[^3]")
 
     def layout(self):
         cmake_layout(self)
